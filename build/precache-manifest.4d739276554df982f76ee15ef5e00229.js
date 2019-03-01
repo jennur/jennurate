dbe699041ebae005764e00240f24b4545fb57391
Jenny Bonsak <jenny.bonsak@gmail.com>
2019-03-01 18:00:30 +0100

    add build folder for deploy

---
+++ build/precache-manifest.4d739276554df982f76ee15ef5e00229.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "90f7d43ac5b71e79cf53",
+    "url": "/static/css/main.dc0d9609.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "a4847d6cc607b4b263ea",
+    "url": "/static/js/2.a4847d6c.chunk.js"
+  },
+  {
+    "revision": "90f7d43ac5b71e79cf53",
+    "url": "/static/js/main.90f7d43a.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "a4847d6cc607b4b263ea",
+    "url": "/static/css/2.53f1ab97.chunk.css"
+  },
+  {
+    "revision": "23f1b9de1e1ff750af270bf68aa8dbd5",
+    "url": "/index.html"
+  }
+];
