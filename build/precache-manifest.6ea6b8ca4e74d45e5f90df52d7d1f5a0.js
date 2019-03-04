53f43cf43e8edf6865f123d97d5c62af1bb1aa9a
Jenny Bonsak <jenny.bonsak@gmail.com>
2019-03-04 12:36:45 +0100

    Update image & build

--- build/precache-manifest.6ea6b8ca4e74d45e5f90df52d7d1f5a0.js
+++ build/precache-manifest.6ea6b8ca4e74d45e5f90df52d7d1f5a0.js
@@ -1,7 +1,7 @@
 self.__precacheManifest = [
   {
-    "revision": "af7ae505a9eed503f8b8e6982036873e",
-    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
   },
   {
     "revision": "ce5695ad682abe938e27",
@@ -24,16 +24,16 @@ self.__precacheManifest = [
     "url": "/static/js/main.ce5695ad.chunk.js"
   },
   {
-    "revision": "fee66e712a8a08eef5805a46892932ad",
-    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
   },
   {
     "revision": "674f50d287a8c48dc19ba404d20fe713",
     "url": "/static/media/fontawesome-webfont.674f50d2.eot"
-  },
-  {
-    "revision": "b06871f281fee6b241d60582ae9369b9",
-    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
   },
   {
     "revision": "d4139c595970c5afa7bc",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b4335731fed78e850484efb88346846b.js"
+  "/precache-manifest.6ea6b8ca4e74d45e5f90df52d7d1f5a0.js"
 );
 
 workbox.clientsClaim();
